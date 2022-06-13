23a7a327b7512c539505362035d458bc9a6568fe
evgeniy kliukvin <evg.klukvin@mail.ru>
2022-06-13 22:59:10 +0700

    шестой урок

---
+++ шестой урок/Program.cs
@@ -0,0 +1,13 @@
+﻿// ветвление
+Console.WriteLine("введите имя пользователя");
+string username = Console.ReadLine();
+
+if("username.ToLoWer() == маша")
+{
+    Console.WriteLine("ура,это же Маша");
+}
+else 
+{ 
+    Console.Write("Привет");
+    Console.WriteLine("username");
+}
