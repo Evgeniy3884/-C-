46a5a890d847a826bb494a1e196c6396ec73495c
evgeniy kliukvin <evg.klukvin@mail.ru>
2022-06-13 22:11:35 +0700

    пятый урок

---
+++ пятый урок/Program.cs
@@ -0,0 +1,7 @@
+﻿// Random случайное число
+int numberA = new Random ().Next(1,10);//123...9
+Console.WriteLine(numberA);
+int numberB = new Random ().Next(1,10);
+Console.WriteLine(numberB);
+int result = (numberA+numberB);
+Console.WriteLine(result);
