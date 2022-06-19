4eb22788d53dec36f72a746c8b668052a30c1698
evgeniy kliukvin <evg.klukvin@mail.ru>
2022-06-19 15:05:36 +0700

    ДЗ. к Семинару 1

---
+++ семинар 1.1/Program.cs
@@ -0,0 +1,27 @@
+﻿//  1.1 Напишите программу, которая на вход
+//принимает два числа и выдаёт, какое число большее, а
+//какое меньшее. 
+
+Console.WriteLine("введите число a");
+ int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число b");
+ int b = Convert.ToInt32(Console.ReadLine());
+
+if( a > b )
+{
+    Console.WriteLine (" max a");
+}
+
+else
+{
+    Console.WriteLine (" max b");
+}
+
+// 1.2 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("введите первое число");
+ int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+ int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите третье число");
+ int с = Convert.ToInt32(Console.ReadLine());
